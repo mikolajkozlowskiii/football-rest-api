318e692206519004a22a1a2f2f8026c751c50af0
mikolajkozlowskiii <mikolajkozlowskiii@gmail.com>
2023-04-24 21:56:41 +0200

    Initial commit

---
+++ src/test/java/com/example/footballrestapi/FootballRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.footballrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
